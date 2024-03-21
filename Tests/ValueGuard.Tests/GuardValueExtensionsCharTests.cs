c0ee7aa5f8356a3070099b8292fc24b9fb1dffc0
Anton Ivanchenko <iv.anton98@gmail.com>
2024-03-21 22:13:04 +0200

    Add GuardValueExtensions methods for char type

---
+++ Tests/ValueGuard.Tests/GuardValueExtensionsCharTests.cs
@@ -0,0 +1,64 @@
+using ValueGuard.Exceptions;
+
+namespace ValueGuard.Tests;
+
+public class GuardValueExtensionsCharTests
+{
+    [Fact]
+    public void IsDigit_Digit_NoException()
+        => Guard.Value('0').IsDigit();
+
+    [Fact]
+    public void IsDigit_Letter_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value('a').IsDigit());
+
+    [Fact]
+    public void IsLetter_Letter_NoException()
+        => Guard.Value('a').IsLetter();
+
+    [Fact]
+    public void IsLetter_Digit_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value('0').IsLetter());
+
+    [Fact]
+    public void IsLetterOrDigit_Digit_NoException()
+        => Guard.Value('0').IsLetterOrDigit();
+
+    [Fact]
+    public void IsLetterOrDigit_Letter_NoException()
+        => Guard.Value('a').IsLetterOrDigit();
+
+    [Fact]
+    public void IsLetterOrDigit_Whitespace_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value(' ').IsLetterOrDigit());
+
+    [Fact]
+    public void IsWhiteSpace_WhiteSpace_NoException()
+        => Guard.Value(' ').IsWhiteSpace();
+
+    [Fact]
+    public void IsWhiteSpace_WhiteSpace_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value('a').IsWhiteSpace());
+
+    [Fact]
+    public void IsUpperCase_UpperChar_NoException()
+        => Guard.Value('A').IsUpperCase();
+
+    [Fact]
+    public void IsUpperCase_LowerChar_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value('a').IsUpperCase());
+
+    [Fact]
+    public void IsLowerCase_LowerChar_NoException()
+        => Guard.Value('a').IsLowerCase();
+
+    [Fact]
+    public void IsLowerCase_UpperChar_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value('A').IsLowerCase());
+}
