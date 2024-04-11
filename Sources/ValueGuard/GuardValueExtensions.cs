d35db131218fae03eaf372b2e4226dccf64617d6
Anton Ivanchenko <iv.anton98@gmail.com>
2024-04-11 23:21:51 +0300

    Add "Is" method to GuardValueExtensions

--- Sources/ValueGuard/GuardValueExtensions.cs
+++ Sources/ValueGuard/GuardValueExtensions.cs
@@ -2,5 +2,18 @@ namespace ValueGuard;
 
 public static partial class GuardValueExtensions
 {
+    public static ref readonly GuardValue<T> Is<T>(this in GuardValue<T> guard, Func<T, bool> predicate)
+        => ref guard.Is(predicate, "The condition is not met for this value");
+
+    public static ref readonly GuardValue<T> Is<T>(this in GuardValue<T> guard, Func<T, bool> predicate, string errorMessage)
+    {
+        if (!predicate.Invoke(guard.Value))
+        {
+            guard.ThrowException(errorMessage);
+        }
+
+        return ref guard;
+    }
+
     public static T GetValue<T>(this in GuardValue<T> guard) => guard.Value;
 }
--- Tests/ValueGuard.Tests/GuardValueExtensionsTests.cs
+++ Tests/ValueGuard.Tests/GuardValueExtensionsTests.cs
@@ -3,7 +3,30 @@ namespace ValueGuard.Tests;
 public sealed class GuardValueExtensionsTests
 {
     [Fact]
+    public void Is_TrueCondition_NoException()
+        => Guard.Value(1).Is(i => i > 0);
+
+    [Fact]
+    public void Is_FalseCondition_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value(1).Is(i => i < 0));
+
+    [Fact]
+    public void Is_TrueConditionWithMessage_NoException()
+        => Guard.Value(1)
+            .Is(i => i > 0, "Value cannot be less or equal zero");
+
+    [Fact]
+    public void Is_FalseConditionWithMessage_ThrowException()
+        => Assert.Throws<GuardException>(()
+            => Guard.Value(1).Is(i => i < 0, "Value must be less zero"));
+
+    [Fact]
     public void GetValue_ReturnOriginalValue()
-        => Assert.Equal("hello world",
-            Guard.NotNull("hello world").GetValue());
+    {
+        const string value = "hello";
+
+        Assert.Equal(value,
+            Guard.NotNull(value).GetValue());
+    }
 }
